13e07fcfb87be7c987b2edebfe98f912bbb1fa18
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-30 18:49:29 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+function success(message) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+function error(message) {
+  const div = document.createElement('div');
+
+  div.classList = 'message error-message';
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+const advicePromise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+advicePromise1.then((reason) => success(reason));
+
+const advicePromise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000, 'Promise was rejected!');
+});
+
+advicePromise2.catch((reason) => error(reason));
